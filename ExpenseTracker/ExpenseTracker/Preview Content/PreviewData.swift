984c72448711268f898b0b269afe560a99b89cb0
chuday <mikhail_chudaew505@mail.ru>
2023-06-17 20:30:20 +0500

    Update logics

---
+++ ExpenseTracker/ExpenseTracker/Preview Content/PreviewData.swift
@@ -0,0 +1,14 @@
+//
+//  PreviewData.swift
+//  ExpenseTracker
+//
+//  Created by Mikhail Chudaev on 17.06.2023.
+//
+
+import Foundation
+import SwiftUI
+
+
+var transactionPreviewData = Transaction(id: 1, date: "01/24/2022", institution: "Desjardins", account: "Visa Desjardins", merchant: "Apple", amount: 11.49, type: "debit", categoryId: 801, category: "Software", isPending: false, isTransfer: false, isExpense: true, isEdited: false)
+
+var transactionListPreviewData = [Transaction](repeating: transactionPreviewData, count: 10)
